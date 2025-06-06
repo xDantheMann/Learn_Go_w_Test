5c489948115586d97b5c71d9a0dcb7c9c9023743
Daniel Mann <danieljeffrey.mann@gmail.com>
2025-06-05 21:02:21 -0400

    added refactors to both main and tests

--- hello.go
+++ hello.go
@@ -1,16 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+const spanish = "Spanish"
+const french = "French"
 
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
+
+	return greetingPrefix(language) + name
+}
+
+/*
+	if language == spanish {
+		return spanishHelloPrefix + name
+	}
+	if language == french {
+		return frenchHelloPrefix + name
+	}
 	return englishHelloPrefix + name
+
+prefix := englishHelloPrefix
+
+switch language {
+case spanish:
+
+	prefix = spanishHelloPrefix
+
+case french:
+
+		prefix = frenchHelloPrefix
+	}
+
+return prefix + name
+*/
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello("Daniel"))
+	fmt.Println(Hello("Daniel", ""))
 }
